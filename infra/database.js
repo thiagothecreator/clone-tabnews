d5cb30c4343605b99872ab2632b4351db12fa26b
thiago santos <87675135+thiagothecreator@users.noreply.github.com>
2025-11-09 03:28:17 +0000

    add ´database.js` and ´.env´ file

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg"
+
+async function query(queryObject) {
+  const client = new Client(
+    {
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT
+    }
+  )
+  await client.connect()
+  const result = await client.query(queryObject)
+  await client.end()
+  return result
+}
+
+export default { 
+  query: query,
+}
