1a49867bc860335dec0e17e5e477ec7c1fd5213d
thiago santos <87675135+thiagothecreator@users.noreply.github.com>
2025-11-15 18:56:08 +0000

    sanitization added to the database.js module

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,25 @@
-import { Client } from "pg"
+import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client(
-    {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      port: process.env.POSTGRES_PORT
-    }
-  )
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+  await client.connect();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
-export default { 
+export default {
   query: query,
-}
+};
