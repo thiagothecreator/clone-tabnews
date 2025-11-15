12dd523ca1c22bb2b7bbf7009cbfdfde173ebfab
thiago santos <87675135+thiagothecreator@users.noreply.github.com>
2025-11-15 18:57:32 +0000

    finish '/api/v1/status' endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
-test('GET to api/v1/status should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status')
+test("GET to api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-})
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const postgresVersion = responseBody.dependencies.database.version;
+  expect(postgresVersion).toEqual("16.0");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toEqual(100);
+
+  const openedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(openedConnections).toEqual(1);
+});
